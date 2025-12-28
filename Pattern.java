7928dcaa7a6e12c18bca1a8230993a28c446e293
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2025-12-28 22:09:58 +0530

    Initial commit

---
+++ Pattern.java
@@ -0,0 +1,460 @@
+public class Pattern {
+    public static void main(String[] args) {
+
+        // 1 2 3 4 5
+        // 2 3 4 5
+        // 3 4 5
+        // 4 5
+        // 5
+        // System.out.println("1st");
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = i; j <= 5; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 1 2 3 4 5
+        // 1 2 3 4
+        // 1 2 3
+        // 1 2
+        // 1
+        // System.out.println("2nd");
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= 5-i+1; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 5 4 3 2 1
+        // 4 3 2 1
+        // 3 2 1
+        // 2 1
+        // 1
+        // System.out.println("3rd");
+        // for (int i = 5; i >= 1; i--) {
+        //     for (int j = i; j >= 1; j--) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 1
+        // 1 2
+        // 1 2 3
+        // 1 2 3 4
+        // 1 2 3 4 5
+        // System.out.println("4th");
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 5
+        // 5 4
+        // 5 4 3
+        // 5 4 3 2
+        // 5 4 3 2 1
+        // System.out.println("5th");
+        // for (int i = 1; i <= 5 ; i++) {
+        //     for (int j = 5; j > 5-i; j--) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 1
+        // 2 2
+        // 3 3 3
+        // 4 4 4 4
+        // 5 5 5 5 5
+        // System.out.println("6th");
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i  ; j++) {
+        //         System.out.print(i+ " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 5
+        // 4 4
+        // 3 3 3
+        // 2 2 2 2
+        // 1 1 1 1 1
+        // System.out.println("7th");
+        // for (int i = 5; i >= 1 ; i--) {
+        //     for (int j = 1; j <= 5-i+1; j++) {
+        //         System.out.print(i + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 1 2 3 4 5
+        // 1 2 3 4
+        // 1 2 3
+        // 1 2
+        // 1
+        // System.out.println("8th");
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= 5-i+1 ; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 5
+        // 5 4
+        // 5 4 3
+        // 5 4 3 2
+        // 5 4 3 2 1
+        // System.out.println("9th");
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 5; j > 5-i; j--) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 1 1 1 1 1
+        // 2 2 2 2
+        // 3 3 3
+        // 4 4
+        // 5
+        // System.out.println("10th");
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= 5-i+1; j++) {
+        //         System.out.print(i + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 5 5 5 5 5
+        // 4 4 4 4
+        // 3 3 3
+        // 2 2
+        // 1
+        // System.out.println("11th");
+        // for (int i = 5; i >= 1; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(i + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // 5 4 3 2 1
+        // 5 4 3 2
+        // 5 4 3
+        // 5 4
+        // 5
+        // System.out.println("12th");
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 5; j >= i ; j--) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        System.out.println("Alphabets Patterns.");
+
+        // A
+        // A B
+        // A B C
+        // A B C D
+        // A B C D E
+        // A B C D E F
+        // System.out.println("13th");
+        // for (int i = 65; i <= 70; i++) {
+        //     for (int j = 65; j <= i ; j++) {
+        //         System.out.print((char)j + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // A
+        // B B
+        // C C C
+        // D D D D
+        // E E E E E
+        // F F F F F F
+        // System.out.println("14th");
+        // for (int i = 65; i <= 70; i++) {
+        //     for (int j = 65; j <= i ; j++) {
+        //         System.out.print((char)i + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // A A A A A
+        // B B B B
+        // C C C
+        // D D
+        // E
+        // System.out.println("15th");
+        // for (int i = 65; i <= 70; i++) {
+        //     for (int j = 1; j <= 70-i ; j++) {
+        //         System.out.print((char)i + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // A
+        // B C
+        // D E F
+        // G H I J
+        // K L M N O
+        // System.out.println("16th");
+        // int num = 65;
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print((char)num + " ");
+        //         num++;
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+
+        // A B C D E
+        // A B C D
+        // A B C
+        // A B
+        // A
+        // System.out.println("17th");
+        // for (int i = 5; i >= 1; i--) {
+        //     int num = 65;
+        //     for (int j = 1; j <= i ; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+        // A
+        // B C
+        // C D E
+        // E F G H
+        // H I J K L
+        // System.out.println("18th");
+        // int num = 65;
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     if(i >= 2) num--;
+        //     System.out.println();
+        // }
+        // System.out.println();
+
+
+        // A B C D E
+        // B C D E F
+        // C D E F G
+        // D E F G H
+        // E F G H I
+        // System.out.println("19th");
+        // int num = 65;
+        // for (int i = 1; i <=5 ; i++) {
+        //     for (int j = 1; j <= 5; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     num = num - 5 + 1;
+        //     System.out.println();
+        // }
+
+        // A
+        // B C
+        // C D E
+        // D E F G
+        // E F G H I
+        // System.out.println("20th");
+        // int num = 65;
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     if( i>=2 ) num = num - i + 1;
+        //     System.out.println();
+        // }
+
+        // A
+        // B C B
+        // C D E D C
+        // D E F G F E D
+        // E F G H I H G F E
+        // System.out.println("21st");
+        // int num = 65;
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //
+        //     if( i>=2 ) {
+        //         num -= 2;
+        //         for (int j = 1; j <= i-1; j++) {
+        //             System.out.print((char)(num--) + " ");
+        //         }
+        //         num +=2;
+        //     }
+        //     System.out.println();
+        // }
+
+        // A
+        // B A
+        // C B A
+        // D C B A
+        // E D C B A
+        // System.out.println("22nd");
+        // int num = 65;
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print((char)(num--) + " ");
+        //     }
+        //     num = num + i + 1;
+        //     System.out.println();
+        // }
+
+        // A
+        // B C
+        // D E F
+        // G H I J
+        // K L M N O
+        // System.out.println("23rd");
+        // int num = 65;
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // A B C D E
+        // A B C D E
+        // A B C D E
+        // A B C D E
+        // A B C D E
+        // System.out.println("24th");
+        // for (int i = 1; i <= 5; i++) {
+        //     int num = 65;
+        //     for (int j = 1; j <= 5; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // A A A A A
+        // B B B B B
+        // C C C C C
+        // D D D D D
+        // E E E E E
+        // System.out.println("25th");
+        // int num = 65;
+        // for (int i = 1; i <= 5 ; i++) {
+        //     for (int j = 1; j <=5 ; j++) {
+        //         System.out.print((char)(num) + " ");
+        //     }
+        //     num++;
+        //     System.out.println();
+        // }
+
+        // A B C D E
+        // F G H I J
+        // K L M N O
+        // P Q R S T
+        // U V W X Y
+        // System.out.println("26th");
+        // int num = 65;
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= 5; j++) {
+        //         System.out.print((char)(num)++ + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //     A
+        //    A B
+        //   A B C
+        //  A B C D
+        // A B C D E
+        // System.out.println("27th");
+        // for (int i = 5; i >= 1; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //
+        //     int num = 65;
+        //     for (int j = 1; j <= 5-i+1; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //     A
+        //    B C
+        //   D E F
+        //  G H I J
+        // K L M N O
+        // System.out.println("28th");
+        // int num = 65;
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 5; j >= i; j--) {
+        //         System.out.print(" ");
+        //     }
+        //
+        //     for (int j = 1; j <=i ; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //    A
+        //   A B
+        //  A B C
+        // A B C D
+        //  A B C
+        //   A B
+        //    A
+        // System.out.println("29th");
+        // int n = 4;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = i; j < n; j++) {
+        //         System.out.print(" ");
+        //     }
+        //
+        //     int num = 65;
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     System.out.println();
+        // }
+        //
+        // for (int i = n - 1; i >= 1; i--) {
+        //     for (int j = i; j < n; j++) {
+        //         System.out.print(" ");
+        //     }
+
+        //     int num = 65;
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print((char)(num++) + " ");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
